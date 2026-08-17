a94d36b8d55d670dc9f09c38327e687df471b333
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-17 08:02:32 +0200

    Add x86_64 interrupt flag helpers

--- kernel/include/boring/cpu.h
+++ kernel/include/boring/cpu.h
@@ -1,10 +1,15 @@
 #ifndef BORING_CPU_H
 #define BORING_CPU_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 uint64_t x86_64_read_cr3(void);
 void x86_64_invalidate_page(uintptr_t virtual_address);
+void x86_64_interrupts_disable(void);
+void x86_64_interrupts_enable(void);
+bool x86_64_interrupts_enabled(void);
+void x86_64_pause(void);
 void x86_64_halt_forever(void) __attribute__((noreturn));
 
 #endif
