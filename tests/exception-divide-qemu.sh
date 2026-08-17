d65dd91b6c8567692c378f44e5f0a6a16a1e3a3d
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-17 08:05:19 +0200

    Keep divide exception acceptance on 0.0.6

--- tests/exception-divide-qemu.sh
+++ tests/exception-divide-qemu.sh
@@ -43,7 +43,7 @@ done
 
 status=0
 for line in \
-    'BoringKernel 0.0.5-dev' \
+    'BoringKernel 0.0.6-dev' \
     'BoringKernel physical memory test passed.' \
     'BoringKernel virtual memory test passed.' \
     'BoringKernel heap test passed.' \
