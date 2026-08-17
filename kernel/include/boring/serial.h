56f37acfe0b051fd09d425e6f2d580aa1891aad1
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-17 06:45:56 +0200

    Add decimal serial output helper

--- kernel/include/boring/serial.h
+++ kernel/include/boring/serial.h
@@ -1,7 +1,10 @@
 #ifndef BORING_SERIAL_H
 #define BORING_SERIAL_H
 
+#include <stdint.h>
+
 void serial_init(void);
 void serial_write_string(const char *text);
+void serial_write_u64(uint64_t value);
 
 #endif
