847037f96e618feceeaec4bb75d383e08c1bd84c
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-17 08:34:12 +0200

    Add x86_64 kernel context ABI

---
+++ kernel/include/boring/context.h
@@ -0,0 +1,22 @@
+#ifndef BORING_CONTEXT_H
+#define BORING_CONTEXT_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+struct x86_64_kernel_context {
+    uint64_t rsp;
+    uint64_t rbx;
+    uint64_t rbp;
+    uint64_t r12;
+    uint64_t r13;
+    uint64_t r14;
+    uint64_t r15;
+};
+
+void x86_64_context_switch(struct x86_64_kernel_context *from,
+                           const struct x86_64_kernel_context *to);
+
+bool x86_64_context_test_callee_saved(void (*yield_fn)(void));
+
+#endif
