98dfea11cc7ee5f45cdc6b9434605342b3d40dad
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-17 09:09:49 +0200

    Expose armed state for full GPR probe

--- kernel/include/boring/context.h
+++ kernel/include/boring/context.h
@@ -20,6 +20,7 @@ void x86_64_context_switch(struct x86_64_kernel_context *from,
 bool x86_64_context_test_callee_saved(void (*yield_fn)(void));
 void x86_64_preemption_probe_reset(void);
 void x86_64_preemption_probe_release(void);
+bool x86_64_preemption_probe_is_armed(void);
 bool x86_64_context_test_preemptive_gprs(void);
 
 #endif
