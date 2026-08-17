73c7058890c786cfd0266115658fa36eba145c5d
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-17 09:18:58 +0200

    Expect BoringKernel 0.0.8 in divide regression

--- tests/exception-divide-qemu.sh
+++ tests/exception-divide-qemu.sh
@@ -43,7 +43,7 @@ done
 
 status=0
 for line in \
-    'BoringKernel 0.0.7-dev' \
+    'BoringKernel 0.0.8-dev' \
     'BoringKernel physical memory test passed.' \
     'BoringKernel virtual memory test passed.' \
     'BoringKernel heap test passed.' \
