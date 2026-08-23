097bd779377e47f4988ded11295a508642da25f8
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-23 21:03:27 +0200

    Add native runtime acceptance interface

---
+++ kernel/include/boring/runtime_test.h
@@ -0,0 +1,13 @@
+#ifndef BORING_RUNTIME_TEST_H
+#define BORING_RUNTIME_TEST_H
+
+#include <stdbool.h>
+
+struct boring_limine_module_response;
+struct x86_64_trap_frame;
+
+bool runtime_test_exception_armed(void);
+void runtime_test_run(const struct boring_limine_module_response *modules);
+void runtime_test_handle_exception(const struct x86_64_trap_frame *frame);
+
+#endif
