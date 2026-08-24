a6a5f313492c850b75a3941ee152265bf8245546
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-24 06:29:04 +0200

    Fix runtime test noreturn contract

--- kernel/include/boring/runtime_test.h
+++ kernel/include/boring/runtime_test.h
@@ -7,7 +7,9 @@ struct boring_limine_module_response;
 struct x86_64_trap_frame;
 
 bool runtime_test_exception_armed(void);
-void runtime_test_run(const struct boring_limine_module_response *modules);
-void runtime_test_handle_exception(const struct x86_64_trap_frame *frame);
+void runtime_test_run(const struct boring_limine_module_response *modules)
+    __attribute__((noreturn));
+void runtime_test_handle_exception(const struct x86_64_trap_frame *frame)
+    __attribute__((noreturn));
 
 #endif
