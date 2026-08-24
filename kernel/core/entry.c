d263522cb64a9d075f31c5cc5720afc1f6537ae9
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-24 06:37:16 +0200

    Bump BoringKernel runtime milestone version

--- kernel/core/entry.c
+++ kernel/core/entry.c
@@ -930,7 +930,7 @@ void boring_kernel_entry(void) {
 
     serial_init();
     serial_write_string("BoringOS booting...\n");
-    serial_write_string("BoringKernel 0.0.12-dev\n");
+    serial_write_string("BoringKernel 0.0.13-dev\n");
     serial_write_string("Arch: x86_64\n");
     serial_write_string("Hello from BoringKernel.\n\n");
 
