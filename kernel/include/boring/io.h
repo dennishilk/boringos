19a5d0fae095f8ad6ee9e6fd8552258f82463a5f
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-08-25 07:25:44 +0200

    Extend x86 port I/O helpers for PCI

--- kernel/include/boring/io.h
+++ kernel/include/boring/io.h
@@ -8,10 +8,34 @@ static inline void x86_64_out8(uint16_t port, uint8_t value) {
     __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
 }
 
+static inline void x86_64_out16(uint16_t port, uint16_t value) {
+    __asm__ volatile ("outw %0, %1" : : "a"(value), "Nd"(port));
+}
+
+static inline void x86_64_out32(uint16_t port, uint32_t value) {
+    __asm__ volatile ("outl %0, %1" : : "a"(value), "Nd"(port));
+}
+
 static inline uint8_t x86_64_in8(uint16_t port) {
     uint8_t value;
     __asm__ volatile ("inb %1, %0" : "=a"(value) : "Nd"(port));
     return value;
+}
+
+static inline uint16_t x86_64_in16(uint16_t port) {
+    uint16_t value;
+    __asm__ volatile ("inw %1, %0" : "=a"(value) : "Nd"(port));
+    return value;
+}
+
+static inline uint32_t x86_64_in32(uint16_t port) {
+    uint32_t value;
+    __asm__ volatile ("inl %1, %0" : "=a"(value) : "Nd"(port));
+    return value;
+}
+
+static inline void x86_64_memory_barrier(void) {
+    __asm__ volatile ("mfence" : : : "memory");
 }
 
 #endif
