3224d75b160aaf8a81d28eee86dee36ddefe5400
Dennis Hilk <81433681+dennishilk@users.noreply.github.com>
2026-09-06 14:46:05 +0200

    M67: give PS2 desktop the same typematic PIT clock

--- kernel/core/m37_desktop_test.c
+++ kernel/core/m37_desktop_test.c
@@ -299,7 +299,8 @@ static bool input_hardware_init(void) {
     struct i8042_state state = {false};
 
     x86_64_interrupts_disable();
-    if (!boring_input_init() || !irq_init()) {
+    if (!boring_input_init() || !irq_init() ||
+        !timer_init(BORING_INPUT_REPEAT_TIMER_HZ)) {
         return false;
     }
     (void)i8042_init(&state);
